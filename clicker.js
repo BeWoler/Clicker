63ea530acd416e21f82c5a02d7e0c8d127a7c31e
BeWoler <bewoler.del@gmail.com>
2021-05-19 09:30:42 +0300

    added restart

--- clicker.js
+++ clicker.js
@@ -5,6 +5,7 @@ const TIMEOUT = 5000;
 const display = document.querySelector("#display");
 const displayCounter = document.querySelector('#display__counter')
 const button = document.querySelector("#button");
+const restartBtn = document.querySelector('#button__restart');
 const counter = document.querySelector("#counter");
 
 button.onclick = start;
@@ -13,7 +14,7 @@ function start() {
   const startTime = Date.now();
 
   display.textContent = formatTime(TIMEOUT);
-  button.onclick = () => (counter.textContent = clicks++);
+  button.onclick = () => (counter.textContent = ++clicks);
 
   const interval = setInterval(() => {
     const delta = Date.now() - startTime;
@@ -23,11 +24,22 @@ function start() {
   const timeout = setTimeout(() => {
     button.onclick = null;
     display.textContent = "Game Over";
-    displayCounter.textContent = `You clicked ${--clicks} times!`
+    displayCounter.textContent = `You clicked ${clicks} times!`;
+
+    restartBtn.style.display = 'block';
+    restartBtn.onclick = restart;
 
     clearInterval(interval);
     clearTimeout(timeout);
   }, TIMEOUT);
+}
+
+function restart() {
+  restartBtn.style.display = 'none';
+  clicks = 0;
+  counter.textContent = clicks;
+  displayCounter.textContent = null;
+  start();
 }
 
 function formatTime(ms) {
