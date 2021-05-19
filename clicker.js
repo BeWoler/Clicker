7f9c397d6b746f7c54ed9ded247e9699b3b25212
BeWoler <bewoler.del@gmail.com>
2021-05-19 08:55:26 +0300

    added time interval

--- clicker.js
+++ clicker.js
@@ -2,19 +2,32 @@ let clicks = 0;
 
 const TIMEOUT = 5000;
 
-const display = document.querySelector('#display');
-const button = document.querySelector('#button');
-const counter = document.querySelector('#counter');
+const display = document.querySelector("#display");
+const button = document.querySelector("#button");
+const counter = document.querySelector("#counter");
 
 button.onclick = start;
 
 function start() {
-  button.onclick = () => counter.textContent = clicks++;
+  const startTime = Date.now();
 
-  const timeout = setTimeout( () => {
+  display.textContent = formatTime(TIMEOUT);
+  button.onclick = () => (counter.textContent = clicks++);
+
+  const interval = setInterval(() => {
+    const delta = Date.now() - startTime;
+    display.textContent = formatTime(TIMEOUT - delta);
+  }, 100);
+
+  const timeout = setTimeout(() => {
     button.onclick = null;
-    display. textContent = 'Game Over';
+    display.textContent = "Game Over";
 
+    clearInterval(interval);
     clearTimeout(timeout);
   }, TIMEOUT);
+}
+
+function formatTime(ms) {
+  return Number.parseFloat(ms / 1000).toFixed(2);
 }
